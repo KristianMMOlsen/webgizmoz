597619447651eccd6438c97108cfc39c16aaa19b
Kristian Olsen <33516139+KristianMMOlsen@users.noreply.github.com>
2021-03-05 12:39:32 +0100

    removed a testcomponent for landingpage+structure

--- src/App.vue
+++ src/App.vue
@@ -22,19 +22,19 @@ export default {
   },
   data() {
     return {
-      Techs: [
+      techs: [
         {
-          navn: "Vue",
-          bilde: "../assets/vue-logo.png",
+          name: "Vue",
+          logo: "../assets/vue-logo.png",
           link: "https://getbootstrap.com/",
-          forklaring:
+          about:
             "Vue is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.",
         },
         {
-          navn: "Bootstrap",
-          bilde: "../assets/bootstrap-logo.png",
+          name: "Bootstrap",
+          logo: "../assets/bootstrap-logo.png",
           link: "https://v3.vuejs.org/",
-          forklaring:
+          about:
             "Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins. ",
         },
       ],
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,44 +1,64 @@
 <template>
   <div class="container">
-      <div class="row mt-3">
-          <img class="LPimg col-9 mx-auto" src="../assets/thinker.jpg" alt="Responsive image">
-      </div>
-      <div class="row">
-          <h1 class="col-12">Velkommen til WebGizmoz, hvor dine web-ideer og behov blir virkeliggjort!</h1>
-      </div>
-      <div class="row mt-3">
-          <img class="LPimg col-9 mx-auto" src="../assets/tinker.jpg" alt="Responsive image">
-      </div>
-      <div class="row">
-          <h1 class="col-12">Ved hjelp av vår gode kompetanse, i tillegg til fremtidsrettet web-teknologi, hjelper vi deg med å utvikle alt fra apper til nettsider.</h1>
-      </div>
-      <div class="row mt-3">
-          <img class="LPimg col-9 mx-auto" src="../assets/vue-logo.png" alt="Responsive image">
-      </div>
-      <div class="row">
-          <h1 class="col-12">Vi er stolte over å utvikle med Vue.js! Vue.js er et trygt og fremtidsrettet rammeverk for web-utvikling, slik at vi kan levere den perfekte løsningen for dine behov.</h1>
-      </div>
+    <div class="row mt-3">
+      <img
+        class="LPimg col-9 mx-auto"
+        src="../assets/thinker.jpg"
+        alt="Responsive image"
+      />
+    </div>
+    <div class="row">
+      <h1 class="col-12">
+        Velkommen til WebGizmoz, hvor dine web-ideer og behov blir
+        virkeliggjort!
+      </h1>
+    </div>
+    <div class="row mt-3">
+      <img
+        class="LPimg col-9 mx-auto"
+        src="../assets/tinker.jpg"
+        alt="Responsive image"
+      />
+    </div>
+    <div class="row">
+      <h1 class="col-12">
+        Ved hjelp av vår gode kompetanse, i tillegg til fremtidsrettet
+        web-teknologi, hjelper vi deg med å utvikle alt fra apper til nettsider.
+      </h1>
+    </div>
+    <div class="row mt-3">
+      <img
+        class="LPimg col-9 mx-auto"
+        src="../assets/vue-logo.png"
+        alt="Responsive image"
+      />
+    </div>
+    <div class="row">
+      <h1 class="col-12">
+        Vi er stolte over å utvikle med Vue.js! Vue.js er et trygt og
+        fremtidsrettet rammeverk for web-utvikling, slik at vi kan levere den
+        perfekte løsningen for dine behov.
+      </h1>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LandingPage',
-  props:{}
-  
-}
+  name: "LandingPage",
+  props: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.container{
-    width: 100%;
-    padding: 0;
-    margin-bottom: 200px;
+.container {
+  width: 100%;
+  padding: 0;
+  margin-bottom: 200px;
 }
-.LPimg{
-    width: 100%;
-    height: auto;
-
+.LPimg {
+  width: 100%;
+  height: auto;
 }
 </style>
